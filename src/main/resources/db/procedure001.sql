723eea05e88f36253d6883bd57b695aaf8a6b43c
romankhiropulos <romankhiropulos@gmail.com>
2022-05-14 21:56:10 +0300

    First version with readme, liquibase enabled true, single procedure

--- src/main/resources/db/procedure001.sql
+++ src/main/resources/db/procedure001.sql
@@ -11,14 +11,3 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE PROCEDURE increment_link_calls_procedure(IN id_url_in BIGINT) AS
-$$
-BEGIN
-
-    UPDATE url
-        SET total_calls = total_calls + 1
-            WHERE id = id_url_in;
-END;
-$$ LANGUAGE plpgsql;
-
-
